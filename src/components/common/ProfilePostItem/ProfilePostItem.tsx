5febaa89da29957000c2b01bcc4a564c8ae25793
RuslanPidhainyi <ruslanpidgauny@gmail.com>
2023-12-30 22:52:44 +0100

    add profile posts components

---
+++ src/components/common/ProfilePostItem/ProfilePostItem.tsx
@@ -0,0 +1,13 @@
+import './style.scss';
+import '../../../style/font.css';
+
+export const ProfilePostItem = () => {
+    return (
+        <div className='single-post'>
+            <h3 className='single-post-title'>Lorem</h3>
+            <div className='single-post-text'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit illum obcaecati architecto nulla adipisci dolores optio tempore molestiae. Veniam eveniet, molestias ad qui distinctio voluptatem autem tenetur id repudiandae quos.
+            </div>
+        </div>
+    )
+}
--- src/components/common/ProfilePostsSection/ProfilePostsSection.tsx
+++ src/components/common/ProfilePostsSection/ProfilePostsSection.tsx
@@ -1,10 +1,14 @@
 import './style.scss';
 import '../../../style/font.css';
+import { ProfilePostItem } from '../ProfilePostItem/ProfilePostItem';
 
 export const ProfilePostsSection = () => {
     return (
         <div className='profilePostsSection'>
-            Posts
+            <ProfilePostItem />
+            <ProfilePostItem />
+            <ProfilePostItem />
+            <ProfilePostItem />
         </div>
     )
 }
